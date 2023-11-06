67f061131f4b79f05c4bae7e6933e36dd3c19f4f
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2023-11-06 22:47:48 +0300

    console.log massage added when delete button clicked

--- src/components/DeleteTodo/DeleteTodo.jsx
+++ src/components/DeleteTodo/DeleteTodo.jsx
@@ -1,8 +1,12 @@
 import { FaRegTrashAlt } from "react-icons/fa";
 
 const DeleteTodo = () => {
+  const deleteTodo = () => {
+    console.log("delete button clicked");
+  };
+
   return (
-    <button>
+    <button onClick={deleteTodo}>
       <FaRegTrashAlt />
     </button>
   );
